af4ef45e89195c804fb062490a018875bdf7c70e
SilentHacks <therealsilenthacks@gmail.com>
2024-03-13 02:15:16 +0000

    Update polarising genres query to reduce duplicated logic

--- app/routers/genres.py
+++ app/routers/genres.py
@@ -38,23 +38,25 @@ async def get_popular(conn: Connection = Depends(get_db_connection)) -> list[dic
 @router.get("/polarising")
 async def get_polarising(conn: Connection = Depends(get_db_connection)) -> list[dict[str, str | float]]:
     genres = await conn.fetch("""
-        SELECT 
-            g.name AS genre,
-            COUNT(*) FILTER (WHERE ur.rating >= 4) * 100.0 / NULLIF(COUNT(ur.rating), 0) AS high_rating_pct,
-            COUNT(*) FILTER (WHERE ur.rating <= 2) * 100.0 / NULLIF(COUNT(ur.rating), 0) AS low_rating_pct
-        FROM 
-            genres g
-        INNER JOIN 
-            movie_genres mg ON g.id = mg.genre_id
-        LEFT JOIN 
-            user_ratings ur ON mg.movie_id = ur.movie_id
-        GROUP BY 
-            g.name
-        HAVING 
-            COUNT(ur.rating) > 0
+        WITH deviations AS (
+            SELECT 
+                g.name AS genre,
+                COUNT(*) FILTER (WHERE ur.rating >= 4) * 100.0 / NULLIF(COUNT(ur.rating), 0) AS high_rating_pct,
+                COUNT(*) FILTER (WHERE ur.rating <= 2) * 100.0 / NULLIF(COUNT(ur.rating), 0) AS low_rating_pct
+            FROM 
+                genres g
+            INNER JOIN 
+                movie_genres mg ON g.id = mg.genre_id
+            LEFT JOIN 
+                user_ratings ur ON mg.movie_id = ur.movie_id
+            GROUP BY 
+                g.name
+            HAVING 
+                COUNT(ur.rating) > 0
+        )
+        SELECT * FROM deviations
         ORDER BY 
-            (COUNT(*) FILTER (WHERE ur.rating >= 4) + 
-            COUNT(*) FILTER (WHERE ur.rating <= 2)) * 100.0 / NULLIF(COUNT(ur.rating), 0) DESC
+            high_rating_pct - low_rating_pct ASC
         LIMIT 10;
     """)
 
