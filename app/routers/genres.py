cb2016bbb05097c6a4388ff1b797dfe0bcc0da8d
SilentHacks <therealsilenthacks@gmail.com>
2024-03-13 06:28:33 +0000

    Add query to get correlation of user personality attributes

--- app/routers/genres.py
+++ app/routers/genres.py
@@ -63,3 +63,51 @@ async def get_polarising(conn: Connection = Depends(get_db_connection)) -> list[
     return [dict(genre) for genre in genres]
 
 
+@router.get("/user_correlation")
+async def get_correlation(conn: Connection = Depends(get_db_connection)) -> list[dict[str, str | float | int]]:
+    genres = await conn.fetch("""
+        WITH avg_rating_per_genre_per_person AS (
+            SELECT
+                ur.user_id,
+                mg.genre_id,
+                AVG(ur.rating) AS avg_rating
+            FROM
+                user_ratings ur
+            INNER JOIN
+                movie_genres mg ON ur.movie_id = mg.movie_id
+            GROUP BY
+                ur.user_id, mg.genre_id
+        ),
+        personality_genre AS (
+            SELECT
+                gr.genre_id,
+                g.name,
+                ps.id,
+                ps.openness,
+                ps.agreeableness,
+                ps.extraversion,
+                ps.emotional_stability,
+                ps.conscientiousness,
+                gr.avg_rating
+            FROM
+                users ps
+            INNER JOIN
+                avg_rating_per_genre_per_person gr ON ps.id = gr.user_id
+            INNER JOIN
+                genres g ON gr.genre_id = g.id
+        )
+        SELECT
+            genre_id,
+            name,
+            corr(openness, avg_rating) AS openness_corr,
+            corr(agreeableness, avg_rating) AS agreeableness_corr,
+            corr(extraversion, avg_rating) AS extraversion_corr,
+            corr(emotional_stability, avg_rating) AS emotional_stability_corr,
+            corr(conscientiousness, avg_rating) AS conscientiousness_corr
+        FROM
+            personality_genre
+        GROUP BY
+            genre_id, name;
+    """)
+
+    return [dict(genre) for genre in genres]
